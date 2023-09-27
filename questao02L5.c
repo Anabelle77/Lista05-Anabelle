a80a498d5d064a87c4312d33256391fa90a95ba4
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2023-09-27 16:18:16 -0300

    Update questao02L5.c

--- questao02L5.c
+++ questao02L5.c
@@ -1,3 +1,6 @@
+//2. Faça um programa que solicite ao usuário para digitar valores numéricos inteiros positivos. Encerre a entrada
+//de dados quando for digitado um número negativo ou zero. Calcule a média dos números positivos digitados.
+
 #include <stdio.h>
 
 int main() {
