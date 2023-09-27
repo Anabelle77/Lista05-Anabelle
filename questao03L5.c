abbd3f7922b029a77d58f0c2cd7fe35e022f5d0a
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2023-09-27 16:17:11 -0300

    Update questao03L5.c

--- questao03L5.c
+++ questao03L5.c
@@ -1,3 +1,6 @@
+//3. Faça um programa que solicite ao usuário 10 números inteiros e, ao final, informe a quantidadede números ímpares e pares lidos. 
+//Calcule também a soma dos números pares e a média dos números ímpares.
+
 #include <stdio.h>
 
 int main() {
